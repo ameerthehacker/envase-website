1a67866dab3d5cc72b4fb150641f5d01ed45b99d
Ameer Jhan <ameerjhanprof@gmail.com>
2021-01-25 19:52:30 +0530

    fix: fallback image

--- components/vimeo/vimeo.tsx
+++ components/vimeo/vimeo.tsx
@@ -6,7 +6,6 @@ export default function Vimeo({ id }: { id: string }) {
       src={`https://player.vimeo.com/video/${id}?background=1`}
       width="100%"
       height="100%"
-      frameBorder="0"
       allow="autoplay; fullscreen; picture-in-picture"
       allowFullScreen
     />
