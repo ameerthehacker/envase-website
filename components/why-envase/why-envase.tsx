05fec547d656c6fa6c5daf75689c5aed5d58a078
Ameer Jhan <ameerjhanprof@gmail.com>
2021-02-06 01:53:02 +0530

    feat: add SEO

--- components/why-envase/why-envase.tsx
+++ components/why-envase/why-envase.tsx
@@ -5,11 +5,18 @@ import Containers from './containers.svg';
 import GH from './github.svg';
 import Code from './code.svg';
 import { FaStar, FaHeart } from 'react-icons/fa';
+import { GITHUB_REPO } from '../../constants';
 
 const RocketSVG = Rocket as any;
 const ContainersSVG = Containers as any;
 const GHSVG = GH as any;
 const CodeSVG = Code as any;
+
+const demos = {
+  mysql: 'https://vimeo.com/500412505',
+  node: 'https://vimeo.com/500468189',
+  fullstack: 'https://vimeo.com/500477155'
+}
 
 export default function WhyEnvase() {
   return (
@@ -27,7 +34,7 @@ export default function WhyEnvase() {
               <Text fontSize="1xl">
                 Launch databases like <strong>MySQL, PostgreSQL, MongoDB</strong> and apps like <strong>VSCode, PhpMyAdmin, PgAdmin</strong> in just few seconds.
               </Text>
-              <Button size="sm" variant="link" marginTop={2}>View Demo</Button>
+              <Button onClick={() => window.open(demos.mysql)} size="sm" variant="link" marginTop={2}>View Demo</Button>
             </Box>
           </Stack>
           <Stack marginTop={10} direction="row" spacing={5}>
@@ -38,7 +45,7 @@ export default function WhyEnvase() {
               <Text fontSize="1xl">
                 Fancy learning a new language like <strong>GoLang, Python or Rust</strong> but lazy to install all the required toolchain? we have got you covered with one click language tooling setup.
               </Text>
-              <Button size="sm" variant="link" marginTop={2}>View Demo</Button>
+              <Button onClick={() => window.open(demos.node)} size="sm" variant="link" marginTop={2}>View Demo</Button>
             </Box>
           </Stack>
           <Stack marginTop={10} direction="row" spacing={5}>
@@ -49,7 +56,7 @@ export default function WhyEnvase() {
               <Text fontSize="1xl">
                 Run fullstack apps like <strong>Hasura, Wordpress, Drupal</strong> with ease and confidence. Envase creates and connects multiple containers over a common network without you ever having to think about it.
               </Text>
-              <Button size="sm" variant="link" marginTop={2}>View Demo</Button>
+              <Button onClick={() => window.open(demos.fullstack)} size="sm" variant="link" marginTop={2}>View Demo</Button>
             </Box>
           </Stack>
           <Stack marginTop={10} direction="row" spacing={5}>
@@ -60,7 +67,7 @@ export default function WhyEnvase() {
               <Text fontSize="1xl">
                 We are <strong>Open Source</strong>, show your love by <FaStar style={{ display: 'inline', color: 'yellow' }} /> the repo. We are also open for contributions <FaHeart style={{ display: 'inline', color: 'red' }} />
               </Text>
-              <a href="https://github.com/ameerthehacker/envase" target="blank">
+              <a href={`https://github.com/${GITHUB_REPO}`} target="blank">
                 <Button size="sm" variant="link" marginTop={2}>View Source</Button>
               </a>
             </Box>
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,36 +2,46 @@ import { CSSReset, ThemeProvider } from '@chakra-ui/core';
 import App from '../components/app/app';
 import fetch from 'isomorphic-fetch';
 import { GITHUB_REPO } from '../constants';
+import { NextSeo } from 'next-seo';
+import absoluteUrl from "next-absolute-url";
 
 export default function Index({
-  latestReleaseTag
+  latestReleaseTag,
+  origin
 }: {
   latestReleaseTag: string;
+  origin: string;
 }) {
   return (
-    <ThemeProvider>
-      <CSSReset />
-      <App latestReleaseTag={latestReleaseTag} />
-    </ThemeProvider>
+    <>
+      <NextSeo
+        title="Envase"
+        description={`Envase removes the pain of running apps using docker by providing an amazing developer experience and an one stop shop app store for almost all popular applications.`}
+        openGraph={{
+          title: "Envase",
+          description: "Envase removes the pain of running apps using docker by providing an amazing developer experience and an one stop shop app store for almost all popular applications.",
+          images: [{
+            url: `${origin}/logo.png`
+          }]
+        }}
+      />
+      <ThemeProvider>
+        <CSSReset />
+        <App latestReleaseTag={latestReleaseTag} />
+      </ThemeProvider>
+    </>
   );
 }
 
-export const getServerSideProps = async () => {
+Index.getIntitialProps = async ({ req }: any) => {
   try {
+    const { origin } = absoluteUrl(req);
     const latestTag = await (
       await fetch(`https://api.github.com/repos/${GITHUB_REPO}/releases/latest`)
     ).json();
 
-    return {
-      props: {
-        latestReleaseTag: latestTag.name
-      }
-    };
+    return { latestReleaseTag: latestTag.name, origin };
   } catch {
-    return {
-      props: {
-        latestReleaseTag: null
-      }
-    };
+    return { latestReleaseTag: null, origin };
   }
 };
