5786175d8ebc1be3985f43b50cd30a7ab106da4c
Ameer Jhan <ameerjhanprof@gmail.com>
2021-02-06 01:13:08 +0530

    feat: add repo details

--- components/why-envase/why-envase.tsx
+++ components/why-envase/why-envase.tsx
@@ -1,12 +1,15 @@
 import { Box, Button, Flex, Stack, Text } from '@chakra-ui/core';
 import Section from '../section/section';
 import Rocket from './rocket.svg';
-import Code from './code.svg';
 import Containers from './containers.svg';
+import GH from './github.svg';
+import Code from './code.svg';
+import { FaStar, FaHeart } from 'react-icons/fa';
 
 const RocketSVG = Rocket as any;
-const CodeSVG = Code as any;
 const ContainersSVG = Containers as any;
+const GHSVG = GH as any;
+const CodeSVG = Code as any;
 
 export default function WhyEnvase() {
   return (
@@ -47,6 +50,19 @@ export default function WhyEnvase() {
                 Run fullstack apps like <strong>Hasura, Wordpress, Drupal</strong> with ease and confidence. Envase creates and connects multiple containers over a common network without you ever having to think about it.
               </Text>
               <Button size="sm" variant="link" marginTop={2}>View Demo</Button>
+            </Box>
+          </Stack>
+          <Stack marginTop={10} direction="row" spacing={5}>
+            <Box>
+              <GHSVG height="70px" />
+            </Box>
+            <Box>
+              <Text fontSize="1xl">
+                We are <strong>Open Source</strong>, show your love by <FaStar style={{ display: 'inline', color: 'yellow' }} /> the repo. We are also open for contributions <FaHeart style={{ display: 'inline', color: 'red' }} />
+              </Text>
+              <a href="https://github.com/ameerthehacker/envase" target="blank">
+                <Button size="sm" variant="link" marginTop={2}>View Source</Button>
+              </a>
             </Box>
           </Stack>
         </Flex>
